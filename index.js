f440514b44203d3014580b4f63316864185bde12
Gonzalo Fredes <gonzafredes1@gmail.com>
2022-08-23 16:27:19 -0300

    Agrego primer logaritmo realizado con ciclo FOR

---
+++ index.js
@@ -0,0 +1,7 @@
+
+let numero = parseInt(prompt("Ingrese un número"))
+
+for (x=1;x<=numero; x++){
+    console.log ("Ciclo de conteo con For: "+x)
+}
+alert ("Puede visualizar el ciclo en su navegador abriendo Herramientas para Desarrollador y luego haciendo click en Consola")
